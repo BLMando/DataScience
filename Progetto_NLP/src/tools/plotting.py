35ad22bd707b96428e942873b1584d7922d1efd9
begbaj <begbaj@proton.me>
2026-01-02 18:59:44 +0100

    ok

--- Progetto_NLP/src/tools/plotting.py
+++ Progetto_NLP/src/tools/plotting.py
@@ -3,14 +3,15 @@ from wordcloud import WordCloud as wc
 from tools import utils as u
 
 
-def wordcloud(wc, df, MAX_WORDS, STOP_WORDS, title="Word Cloud of Top Words"):
+def wordcloud(wc, df, MAX_WORDS, STOP_WORDS, title=None):
 
     wc.generate_from_frequencies(dict(u.top_in_series(df, MAX_WORDS, STOP_WORDS)))
 
     fig = plt.figure(figsize=(10, 5))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off') # Remove axes for a cleaner look
-    plt.title(title, fontsize=14, pad=20)
+    if title:
+        plt.title(title, fontsize=14, pad=20)
     plt.tight_layout()
     plt.close()
     return fig
